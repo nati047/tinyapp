b12e41739e103fbf74e9cd3b4201474dbf58940b
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-08 22:07:15 +0000

    refactor express_server.js

--- express_server.js
+++ express_server.js
@@ -22,8 +22,8 @@ app.get('/urls', (req, res) =>{
   res.render('urls_index', templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: 'user input' };
-  res.render("urls_show", templateVars);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars );
 });
 
 app.listen(PORT, () => {
