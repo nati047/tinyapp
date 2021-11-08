169b0f0a73e446a8ee80b866194c552983e5001f
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-08 21:34:51 +0000

    fill out urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +17,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get('/urls', (req, res) =>{
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
